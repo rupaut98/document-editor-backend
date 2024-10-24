dc4f76529141b7c8793fdcca1d4aaebf7e027593
rupaut98 <rupakraut78@gmail.com>
2024-10-24 00:14:20 -0500

    Unique username

--- src/Model.hs
+++ src/Model.hs
@@ -1,4 +1,15 @@
+-- src/Model.hs
 {-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TemplateHaskell #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE DerivingStrategies #-}
+{-# LANGUAGE StandaloneDeriving #-}
+
 module Model where
 
 import Data.Aeson (FromJSON, ToJSON)
@@ -6,19 +17,24 @@ import GHC.Generics (Generic)
 import Database.Persist.TH
 import Data.Text (Text)
 
--- Document data type
-data Document = Document
-    { docId :: Int
-    , docTitle :: String
-    , docContent :: String
-    } deriving (Show, Generic)
-
+-- Define the User model with a unique constraint on username
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
 User
     username Text
     password Text -- This will store the hashed password
+    UniqueUsername username
+    deriving Show Generic
+
+Document
+    title Text
+    content Text
+    ownerId UserId
     deriving Show Generic
 |]
 
 instance FromJSON Document
 instance ToJSON Document
+
+-- Additional instances for User if needed
+instance FromJSON User
+instance ToJSON User
